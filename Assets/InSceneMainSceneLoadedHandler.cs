f4fa81de0555dbdf091669534f2adb8ddaf4d65b
STARasGAMES <doterpoter@gmail.com>
2021-05-20 11:47:15 +0300

    main. Added samples

--- Assets/InSceneMainSceneLoadedHandler.cs
+++ Assets/InSceneMainSceneLoadedHandler.cs
@@ -1,11 +1,29 @@
-﻿using SaG.MainSceneAutoLoading;
+﻿#if UNITY_EDITOR // this script should not be present in builds
+using System.Collections;
+using SaG.MainSceneAutoLoading;
 using SaG.MainSceneAutoLoading.MainSceneLoadedHandlers;
+using SaG.MainSceneAutoLoading.Utilities;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InSceneMainSceneLoadedHandler : MonoBehaviour, IMainSceneLoadedHandler
 {
     public void OnMainSceneLoaded(LoadMainSceneArgs args)
     {
-        Debug.Log($"[InSceneMainSceneLoadedHandler] OnMainSceneLoaded");
+        Debug.Log($"OnMainSceneLoaded! Now decide what to do with args.SceneSetups...");
+        StartCoroutine(LoadDesiredScenes(args));
+    }
+
+    IEnumerator LoadDesiredScenes(LoadMainSceneArgs args)
+    {
+        yield return new WaitForSeconds(1f);
+        foreach (var sceneSetup in args.SceneSetups)
+        {
+            SceneManager.LoadScene(sceneSetup.path, LoadSceneMode.Additive);
+        }
+
+        // call this to restore previously selected and expanded GameObjects 
+        SceneHierarchyStateUtility.RestoreHierarchyState(args);
     }
 }
+#endif
---
+++ Assets/Sample_MainSceneProvider.cs
@@ -0,0 +1,53 @@
+﻿using SaG.MainSceneAutoLoading.MainSceneProviders;
+using UnityEditor;
+using UnityEngine;
+
+public class Sample_MainSceneProvider : IMainSceneProvider
+{
+    [SerializeField]
+    private bool _setting1;
+
+    [SerializeField]
+    private int _setting2;
+
+    [SerializeField]
+    private Object _setting3;
+
+    public SceneAsset Get()
+    {
+        // implementation
+        throw new System.NotImplementedException();
+    }
+
+    [CustomPropertyDrawer(typeof(Sample_MainSceneProvider))]
+    public class Sample_MainSceneProviderPropertyDrawer : PropertyDrawer
+    {
+        private const int FieldsCount = 4;
+        private const int FieldHeightSelf = 18;
+        private const int FieldHeightTotal = 20;
+
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return FieldHeightTotal * FieldsCount;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.indentLevel++;
+
+            position.height = FieldHeightSelf;
+            GUI.enabled = false;
+            EditorGUI.LabelField(position, "Custom description");
+            GUI.enabled = true;
+            position.y += FieldHeightTotal;
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("_setting1"));
+            position.y += FieldHeightTotal;
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("_setting2"));
+            position.y += FieldHeightTotal;
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("_setting3"));
+
+            EditorGUI.indentLevel--;
+        }
+    }
+}
