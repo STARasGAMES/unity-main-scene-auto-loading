013f5d03332fcbd24d27f9707a3f85139b70f20b
STARasGAMES <doterpoter@gmail.com>
2021-05-21 12:39:50 +0300

    F-1. Added serialization for LoadMainScenArgs. Fixed issues caused by domain reload.

--- Packages/SaG.MainSceneAutoLoading/Editor/LoadMainSceneArgs.cs
+++ Packages/SaG.MainSceneAutoLoading/Editor/LoadMainSceneArgs.cs
@@ -1,31 +1,82 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.SceneManagement;
+using UnityEngine;
 
 namespace SaG.MainSceneAutoLoading
 {
     public class LoadMainSceneArgs
     {
         public readonly SceneSetup[] SceneSetups;
-        public readonly GlobalObjectId[] SelectedObjectsInHierarchy;
-        public readonly GlobalObjectId[] UnfoldedObjects;
+        public readonly GlobalObjectId[] SelectedInHierarchyObjects;
+        public readonly GlobalObjectId[] ExpandedInHierarchyObjects;
         public readonly List<string> ExpandedScenes;
-        public readonly bool RestoreHierarchyState;
 
         public LoadMainSceneArgs(
             SceneSetup[] sceneSetups,
-            GlobalObjectId[] selectedObjectsInHierarchy,
-            GlobalObjectId[] unfoldedObjects,
-            List<string> expandedScenes,
-            bool restoreHierarchyState)
+            GlobalObjectId[] selectedInHierarchyObjects,
+            GlobalObjectId[] expandedInHierarchyObjects,
+            List<string> expandedScenes)
         {
             SceneSetups = sceneSetups;
-            SelectedObjectsInHierarchy = selectedObjectsInHierarchy;
-            UnfoldedObjects = unfoldedObjects;
+            SelectedInHierarchyObjects = selectedInHierarchyObjects;
+            ExpandedInHierarchyObjects = expandedInHierarchyObjects;
             ExpandedScenes = expandedScenes;
-            RestoreHierarchyState = restoreHierarchyState;
         }
 
         // todo Serialization to save in EditorPrefs
+
+        [System.Serializable]
+        private class SaveData
+        {
+            public SceneSetup[] SceneSetups;
+            public string[] SelectedInHierarchyObjects;
+            public string[] ExpandedInHierarchyObjects;
+            public List<string> ExpandedScenes;
+        }
+
+        public string Serialize()
+        {
+            var saveData = new SaveData()
+            {
+                SceneSetups = SceneSetups,
+                SelectedInHierarchyObjects = SelectedInHierarchyObjects.Select(x => x.ToString()).ToArray(),
+                ExpandedInHierarchyObjects = ExpandedInHierarchyObjects.Select(x => x.ToString()).ToArray(),
+                ExpandedScenes = ExpandedScenes
+            };
+
+            var json = JsonUtility.ToJson(saveData);
+            return json;
+        }
+
+        public static LoadMainSceneArgs Deserialize(string json)
+        {
+            var saveData = JsonUtility.FromJson<SaveData>(json);
+            
+            var args = new LoadMainSceneArgs(
+                saveData.SceneSetups,
+                ParseGlobalObjectIds(saveData.SelectedInHierarchyObjects),
+                ParseGlobalObjectIds(saveData.ExpandedInHierarchyObjects),
+                saveData.ExpandedScenes
+                );
+
+            return args;
+        }
+
+        private static GlobalObjectId[] ParseGlobalObjectIds(string[] stringIds)
+        {
+            var ids = new List<GlobalObjectId>(stringIds.Length);
+            foreach (var stringId in stringIds)
+            {
+                if (GlobalObjectId.TryParse(stringId, out var id))
+                {
+                    ids.Add(id);
+                }
+            }
+
+            return ids.ToArray();
+        }
+        
     }
 }
